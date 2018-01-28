07714aee2e5413121da7f5b58c2bcc7270275dcc
niruku7 <niruku7@gatech.edu>
2018-01-28 13:43:43 -0500

    Wrote the calc file and updated Person 1.

--- app/src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ app/src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Nyambura Iruku
  *  @version 1.1
  */
 public class Person1 {
@@ -31,7 +31,9 @@ public class Person1 {
 	 */
 	private String calc(String input) {
 	  //Person 1 put your implementation here
-	  return null;
+		String temp = input;
+	  String temp2 = temp.substring(1) + temp.at(0);
+	  return temp2.substring(1) + temp.at(0);
 	}
 	
 	/**
